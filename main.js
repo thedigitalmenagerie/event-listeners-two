b098ef59768d92eac804f171839ba41528ef4a22
Honey-Rae Swan <honeyraeswan@gmail.com>
2021-01-31 09:13:16 -0600

    event-listeners

--- main.js
+++ main.js
@@ -1,77 +1,59 @@
+// event-listeners a function that fires when an event occurs on the DOM-- a click, a mouse entering an area, an image download finishing, etc
 
-// dom -- document object model // global object // what makes everything show in the browser // document that makes the model for the driver
+// event-listeners list do a search! mdn and w3 schools
 
-// something happens === manipulating the dom
+// const div = document.querySelector('#main-btn'); // selecting the element from html we want to target
 
-// callbacks-- calling a function based on a user action that cues the start
+// div.addEventListener('click', () => { // target the variable from above and add an event listener
+//   console.log('button is clicked'); // first arg is what event it is looking for and the second argument is the function that makes the next thing happen
+// });
 
-// event-listeners-- events that happen on the browser that the dom is listening for
+// this is where cb come in
+// const button = document.getElementById('main-btn'); // targeting the button from html
 
-// bootstrap-- css library with built in html and css for quicker development
+// // we know how to make a function and we know how to use a function using a callback and addEventListener is just a function
+// // you can add an event listener to anything you target on the dom
 
-// method getElementById to add functionality in JS
-
-// const div = document.getElementById('divId'); // selector that passes the id of the element (id) we want to target from html as a string
-
-// document selectors
-
-// const button = document.getElementById('main-btn');
-
-// console.log(button);
-
-// document.querySelector 
-
-// const div = document.querySelector('#divId'); // can target classes or id and is the new way . for class instead of # 
-// will only get the first one and should be that way because you can only have specific ids
-// should not be using classes in js because ids are for js and classes are for css
-
-// const button = document.querySelector('#main-btn');
-
-// console.log(button);
-
-// callbacks-- ability to pass a function as an argument to another function 
-//convention is to use cb argument variable as you callback
-
-// function saysHelloToUser(user) { // passing an argument and use argument inside the code block (user is arg)
-//   return `Hello ${user}!`;
+// const whatIsTheIds = (e) => { // the argument being passed here is e or event
+//   return `The element ID is ${e.target.id}!`; 
 // }
 
-// function saysGoodbyeToUser(user) { // user === taco and is whatever we use to call taco
-//   return `Goodbye ${user}!`; // only difference is the return
-// }
+// button.addEventListener('click', whatIsTheId);
 
-// // call back
+const button = document.querySelector('#main-btn'); // targeting this element on the DOM
+const container = document.querySelector('#container'); 
+// button.addEventListener('click', (e) => { // adding an event listener to this button // passing an e/event 
+//   console.log('You clicked me!'); // nothing happens without targeting second element of other-btn 
+//   console.log(e); // when you add e it logs You clicked me! and actual event (detailed list comes down in dev tools)
+// }) // hard code
 
-// function createGreeting (user, cb) { // user == taco/arg/placeholder 2nd arg is cb === taco/placeholder
-//   return cb(user); // a function can be passed as an argument in another function and that is a CB
-// }
+// button.addEventListener('click', (e) => { // adding an event listener to this button // passing an e/event 
+//   console.log(`You clicked me ${e.target.id}`); // nothing happens without targeting second element of other-btn // interpolate ${}
+//   console.log(e.altKey); // when you add e it logs You clicked me! and actual event (detailed list comes down in dev tools of objects we can access on the event)
+// }) // e.target.id === element of the id we created // hard code
 
-// // doesn't have to be invoked // it is being passed not invoking which would be inside of the code block
+// what is the issue with using this function inside the addEventListener is it is not reusable bc it only targets one button
+// cb would target more than one
 
-// createGreeting('Dan', saysHelloToUser); // 'Hello Dan!'
-// createGreeting('Dan', saysGoodbyeToUser); // 'Goodbye Dan!'
+const whatIsTheId = (e) => { // passing e 
+  console.log(`You clicked ${e.target.id}!`); // 
+}
 
-// create two functions that accept a string as an arg and returns a sentence using the argument
+if (e.target.id === 'main-btn') { // put all selectors at top
+  container.innerHTML = 'You clicked the main-btn'; // puts this inside the div #container // will replace existing text or create text
+} else {
+  container.innerHTML = 'You clicked the other-btn'; // puts this inside the div #container else would make all other buttons log this
+}
 
-// function one(string) {
-//   return `This is a ${string}.`;
-// }
+button.addEventListener('click', whatIsTheId); // one resuable cb function that can be reused
 
-// function two(string) {
-//   return `This is a second ${string}.`;
-// }
+const button2 = document.querySelector('#other-btn'); // targets 2nd button
 
-// // create a third function that accepts the two arguments (a string and a function) and returns the passed function with the string as the argument
-// // a function that takes a funtion in as an argument
-// function third(string, cb) {
-//   return cb(string);
-// }
+button2.addEventListener('click', whatIsTheId); // uses just first and last because whatIsTheId function is reusable 
 
-// // console.log the third function twice using the first two functions as arguments
-// //console logging the return statement of each callback
+// can add an event listener on anything you have on the dom
+// you can hard code as a arg or you can use a cb
 
-// third('Dan', first); // format is accepting two arguments one as a function
-// third('Dan', second); // allows us to make code reusable by having one cb use multiple functions
+// target events click is an event and there is a list of them that you can pass as a function
 
-// // callbacks are used in DOM manipulation and events/ event-listeneres which are listening for functions and cb
-
+// adding logic based on what they clicked
